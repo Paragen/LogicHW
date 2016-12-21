ddfd477a2497dd0cf27b74f0a7cd8f5318a2c48c
Paragen <k-paradox-p@mail.ru>
2016-12-21 15:35:54 +0300

    fix

--- ru/ifmo/ctddev/paragen/mathLogic/Tester.java
+++ ru/ifmo/ctddev/paragen/mathLogic/Tester.java
@@ -9,6 +9,25 @@ import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
+        if (args.length >=3 ) {
+            switch (Integer.parseInt(args[0])) {
+                case 1:
+                    new ExpressionParser().run(args[1],args[2]);
+                    break;
+            case 2:
+                    new Task2().run(args[1],args[2]);
+                    break;
+            case 3:
+                    new Task3().run(args[1],args[2]);
+                    break;
+            case 4:
+                    new Task4().run(args[1],args[2]);
+                    break;
+                case 5:
+                    new Task5().run(args[1],args[2]);
+                    break;
+            }
+        }
         final String in = "/home/ouroboros/gitRepositories/logic2014/tests/" , out = "/home/ouroboros/answers/";
         long inTime ;
         int num = 1;
